716b5e99707deaecd8aac7c56b32217ab1955081
Haleigh <haleighvalentine@gmail.com>
2024-10-08 16:39:55 +0200

    Initialise files

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import pluginReact from 'eslint-plugin-react';
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
+
+export default [
+  { files: ['**/*.{js,mjs,cjs,jsx}'] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  eslintPluginPrettier,
+  {
+    rules: {
+      'capitalized-comments': ['error', 'always'],
+    },
+  },
+];
