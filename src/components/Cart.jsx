f479f251e1c59d55cb428750a70de5cf95302170
Haleigh <haleighvalentine@gmail.com>
2024-10-12 04:20:47 +0200

    Add logic and styling to Cart component

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,13 +1,82 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import CartContext from '../store/cartContext';
 
 export default function Cart() {
+  const cartCtx = useContext(CartContext);
+
+  const totalCartProducts = cartCtx.products.reduce(
+    (totalNumberOfProducts, products) => {
+      return totalNumberOfProducts + products.quantity;
+    },
+    0,
+  );
+
+  const totalPrice = cartCtx.products.reduce(
+    (totalPriceOfProducts, products) => {
+      return totalPriceOfProducts + products.quantity * products.price;
+    },
+    0,
+  );
+
   return (
     <aside className="cart-container">
-      <p className="text2 red">Your Cart(0)</p>
-      <div>
-        <img src="/images/illustration-empty-cart.svg" alt="" />
-        <p className="text4-bold rose500">Your added item will appear here</p>
-      </div>
+      <p className="text2 red">Your Cart ({totalCartProducts})</p>
+      {cartCtx.products.length === 0 ? (
+        <div className="center">
+          <img
+            src="/images/illustration-empty-cart.svg"
+            alt=""
+            className="center"
+          />
+          <p className="text4-bold rose500 py16">
+            Your added item will appear here
+          </p>
+        </div>
+      ) : (
+        <>
+          <ul className="cart-items">
+            {cartCtx.products.map((product, index) => {
+              return (
+                <li key={index} className="cart-item">
+                  <div className="">
+                    <p className="text4-bold rose900">{product.name}</p>
+                    <div className="cart-item__details">
+                      <p className="text4-bold red">{product.quantity}x</p>
+                      <p className="text4 rose500">
+                        @${product.price.toFixed(2)}
+                      </p>
+                      <p className="text4-bold rose500">
+                        ${(product.price * product.quantity).toFixed(2)}
+                      </p>
+                    </div>
+                  </div>
+                  <button
+                    className="remove-item-btn"
+                    onClick={() => cartCtx.removeProduct(product.name)}
+                  >
+                    <img
+                      src="/public/images/icon-remove-item.svg"
+                      alt="Remove Item"
+                    />
+                  </button>
+                </li>
+              );
+            })}
+          </ul>
+          <div className="order-total">
+            <p className="text4 rose900">Order Total</p>
+            <p className="text2 rose900">${totalPrice.toFixed(2)}</p>
+          </div>
+          <div className="carbon-free">
+            <img src="/public/images/icon-carbon-neutral.svg" alt="" />
+            <p className=" text4 rose900">
+              This is a{' '}
+              <span className="text4-bold rose900">carbon-neutral</span>delivery
+            </p>
+          </div>
+          <button className="confirm-order-btn text3">Confirm Order</button>
+        </>
+      )}
     </aside>
   );
 }
